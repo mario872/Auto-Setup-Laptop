5efc80c2d4527fc3dc24f543b34e8d0ced4222b8
mario872 <106939571+mario872@users.noreply.github.com>
2022-12-19 18:29:29 +1100

    Add files via upload

---
+++ Install and Setup.bat
@@ -0,0 +1,16 @@
+powershell.exe -c "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+powershell.exe -c "[system.Diagnostics.Process]::Start('firefox','https://activate.movavi.com/eds.php?ouid=eb9e950826a8b4a1c4d1599c4b0a5c93&lang=en')"
+choco install adobereader -y
+choco install firefox -y
+choco install jre8 -y
+choco install python3 -y
+choco install 7zip.install -y
+choco install vlc -y
+choco install cider -y
+choco install powertoys -y
+choco install office365homepremium -y
+choco install thonny -y
+choco install visualstudio2022community -y
+choco install cura -y
+choco install rpi-imager
+choco install onedrive -y
