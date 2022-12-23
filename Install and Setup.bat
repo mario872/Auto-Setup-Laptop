91fce1c9b69f2516f8b268244747cc12c5029e53
mario872 <106939571+mario872@users.noreply.github.com>
2022-12-24 08:03:09 +1100

    Update Install and Setup.bat
    Changed some apps, also this is my 3rd clean install in one week of win 11.

--- Install and Setup.bat
+++ Install and Setup.bat
@@ -1,6 +1,5 @@
 powershell.exe -c "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
-powershell.exe -c "[system.Diagnostics.Process]::Start('firefox','https://activate.movavi.com/eds.php?ouid=[PRODUCTIDHERE]&lang=en')"
-choco install adobereader -y
+# powershell.exe -c "[system.Diagnostics.Process]::Start('firefox','https://activate.movavi.com/eds.php?ouid=[PRODUCTIDHERE]&lang=en')"
 choco install firefox -y
 choco install jre8 -y
 choco install python3 -y
@@ -9,7 +8,9 @@ choco install vlc -y
 choco install cider -y
 choco install powertoys -y
 choco install thonny -y
-choco install visualstudio2022community -y
 choco install cura -y
-choco install rpi-imager
-choco install onedrive -y
+choco install rpi-imager -y
+choco install minecraft-launcher -y
+choco install adobereader -y
+choco install microsoft-teams.install -y
+choco install atom -y
