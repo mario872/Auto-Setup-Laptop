ac5d4b9392a5996f3c768757765687c39e8f1dce
mario872 <106939571+mario872@users.noreply.github.com>
2022-12-24 11:51:39 +1100

    Update Install and Setup.bat

--- Install and Setup.bat
+++ Install and Setup.bat
@@ -1,4 +1,5 @@
 powershell.exe -c "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+mkdir C:\Users\James\Pictures\Wallpaper
 powershell.exe -c "Invoke-WebRequest -OutFile C:\Users\James\Pictures\Wallpaper\Mountain.jpeg -Uri https://static-community.frame.work/original/2X/6/6dba4dfb5d3ef40c5c49e116a913fd1c3a8fbcdd.jpeg"
 powershell.exe -c "Invoke-WebRequest -OutFile C:\Users\James\Pictures\Wallpaper\Bridge.jpeg -Uri https://static-community.frame.work/original/2X/c/c0ccd40063d3851b4f32a1c10b89c96d97fb297f.jpeg"
 powershell.exe -c "Invoke-WebRequest -OutFile C:\Users\James\Pictures\Wallpaper\Green.jpeg -Uri https://static-community.frame.work/original/2X/8/804c88c3f2c616e4455b566c62050dd37d73b7fc.jpeg"
